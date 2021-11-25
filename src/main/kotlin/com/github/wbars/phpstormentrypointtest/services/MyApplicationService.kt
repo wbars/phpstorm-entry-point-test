a6983d004f1e1bd587df8adcbce2e8e52a48bd15
GitHub Action <action@github.com>
2021-11-25 10:44:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wbars/phpstormentrypointtest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wbars.phpstormentrypointtest.services
+
+import com.github.wbars.phpstormentrypointtest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/wbars/phpstormentrypointtest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wbars/phpstormentrypointtest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wbars.phpstormentrypointtest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wbars.phpstormentrypointtest.MyBundle
 
 class MyProjectService(project: Project) {
 
